926c2632f3a366152abf04334a5a76e165b8e5d4
raimaimran <raima.imran.ri@gmail.com>
2019-11-25 22:29:33 +0500

    other tests

--- app/src/androidTest/java/com/example/presentation/signInTest.java
+++ app/src/androidTest/java/com/example/presentation/signInTest.java
@@ -1,23 +1,35 @@
 package com.example.presentation;
 
 import android.app.Activity;
+import android.widget.Toast;
 
 import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
+import androidx.test.espresso.remote.EspressoRemoteMessage;
 import androidx.test.ext.junit.runners.AndroidJUnit4; //for junit class
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.provider.ContactsContract.Directory.PACKAGE_NAME;
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class signInTest {
@@ -44,20 +56,71 @@ public class signInTest {
     }
 
     @Test
-    public void button_redirects_to_homepage(){
-        onView(withId(R.id.bssignin)) //this checks whether button can be pressed, matches the button with id and clicks it
-                .perform(click());
-        onView(withId(R.id.homePage)) //second find the view displayed and checks whether it is displayed or not
-                .check(matches(isDisplayed()));
-    }
-
-    @Test
     public void link_redirects_to_signup(){
         onView(withId(R.id.redirectsignup))
                 .perform(click());
 
         onView(withId(R.id.signUp))
                 .check(matches(isDisplayed()));
+    }
+
+
+
+    @Test
+    public void button_redirects_to_homepage(){
+
+        onView(withId(R.id.bsemailid))
+                .perform(typeText("raima.imran@gmail.com"));
+        onView(withId(R.id.bspassword))
+                .perform(typeText("password"));
+
+        closeSoftKeyboard();
+        onView(withId(R.id.bssignin)) //this checks whether button can be pressed, matches the button with id and clicks it
+                .perform(click());
+
+        onView(withId(R.id.homePage)) //second find the view displayed and checks whether it is displayed or not
+                .check(matches(withText("Welcome to your HomePage raima.imran@gmail.com" )));
+
+    }
+
+
+    @Test
+    public void verify_correct_email_received_by_activity(){
+
+        onView(withId(R.id.bsemailid))
+                .perform(typeText("raima.imran@gmail.com"));
+        onView(withId(R.id.bspassword))
+                .perform(typeText("password"));
+
+        closeSoftKeyboard();
+        
+        onView(withId(R.id.bssignin))
+                .perform(click());
+
+        intended(allOf(
+                hasComponent(hasShortClassName(".homePage")),
+                toPackage("com.example.presentation"),
+                IntentMatchers.hasExtra("MainActivity.ExtraData", "Welcome to your HomePage raima.imran@gmail.com")));
+    }
+
+
+
+    @Test
+    public void error_displayed_when_wrong_email_entered(){
+        onView(withId(R.id.bsemailid))
+                .perform(typeText("abc.gmail.com"));
+        onView(withId(R.id.bspassword))
+                .perform(typeText("password"));
+
+        closeSoftKeyboard();
+
+        onView(withId(R.id.bssignin))
+                .perform(click());
+
+        onView(allOf(withId(Toast.LENGTH_SHORT), isDisplayed())).check(matches(withText("You must enter a valid email.")));
+
+        //all of is a combination function
+        //in this case first it catches all toast with the given id and then selects the toast displayed with the following texts
     }
 
 }
--- app/src/main/java/com/example/presentation/homePage.java
+++ app/src/main/java/com/example/presentation/homePage.java
@@ -3,6 +3,7 @@ package com.example.presentation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class homePage extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class homePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        TextView tv;
+        tv = (TextView) findViewById(R.id.homePage);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String uname = extras.getString("UserName");
+            //The key argument here must match that used in the other activity
+            tv.setText("Welcome to your HomePage " + uname);
+        }
+
     }
 }
